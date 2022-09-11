69cfe649a04559aced1c520e976f21afef9e2e8c
Hammad <hammad2091504@hotmail.com>
2022-09-11 12:27:40 +0000

    Add coverage, pip3 install coverage >coverage run --source=todo manage.py test> coverage html

--- todo/tests_views.py
+++ todo/tests_views.py
@@ -10,9 +10,9 @@ class TestViews(TestCase):
         self.assertTemplateUsed(response, 'todo/todo_list.html')
 
     def test_get_add_item_page(self):
-        response = self.client.get('/')
+        response = self.client.get('/add')
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'todo/todo_list.html')
+        self.assertTemplateUsed(response, 'todo/add_item.html')
 
     def test_get_edit_item_page(self):
         item = Item.objects.create(name='Test Todo Item')
@@ -31,10 +31,18 @@ class TestViews(TestCase):
         existing_items = Item.objects.filter(id=item.id)
         self.assertEqual(len(existing_items), 0)
 
-
     def test_can_toggle_view(self):
         item = Item.objects.create(name='Test Todo Item', done = True)
         response = self.client.get(f'/toggle/{item.id}')
         self.assertRedirects(response, '/')
         updated_item = Item.objects.get(id=item.id)
         self.assertFalse(updated_item.done)
+    
+    def test_can_edit_item(self):
+        item = Item.objects.create(name='Test Todo Item')
+        response = self.client.post(f'/edit/{item.id}', {'name' : 'Updated Name'})
+        self.assertRedirects(response, '/')
+        updated_item = Item.objects.get(id=item.id)
+        self.assertEqual(updated_item.name, 'Updated Name')
+
+
